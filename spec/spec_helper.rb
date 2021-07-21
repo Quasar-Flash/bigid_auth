33f2efd9f9ac0850fd3a65f3cc81f860ed6255cd
Danilo Carolino <danilogcarolino@gmail.com>
2021-07-21 20:08:01 -0300

    feat: use the flash_integration to retrieve the connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
+require "simplecov"
+
+SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter]
+SimpleCov.minimum_coverage 70.0
+
+SimpleCov.start do
+  add_filter "/spec/"
+  minimum_coverage 70
+  minimum_coverage_by_file 40
+***REMOVED***
+
 require "rubygems"
 require "bundler"
 require "bigid_auth"
+require "pry"
 
 begin
   Bundler.setup(:default, :development, :test)
@@ -13,14 +25,13 @@ rescue Bundler::BundlerError => e
   exit e.status_code
 ***REMOVED***
 
-require "simplecov"
-
-SimpleCov.start do
-  add_filter "spec"
-***REMOVED***
-
 RSpec.configure do |config|
-  # some (optional) config here
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  ***REMOVED***
 ***REMOVED***
 
 # minitest/mock # Uncomment me to use minitest mocks
